c9c2509c8366e7b2ae4324bde4f446e0e053f8b0
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2023-08-16 21:45:53 +0700

    fix utils directory

--- src/states/product/action.js
+++ src/states/product/action.js
@@ -1,6 +1,6 @@
 import { toast } from "react-toastify";
 import { ActionType } from "../../constants/ActionType";
-import api from "../../../utils/api";
+import api from "../../utils/api";
 
 const receiveProductActionCreator = (product) => ({
   type: ActionType.RECEIVE_PRODUCT,
